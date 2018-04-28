dca2a3633b4bbe14f201433d8cf942acfaaa1e3c
AwesomeTy79 <32660892+AwesomeTy79@users.noreply.github.com>
2018-04-27 21:41:50 -0600

    Update random.js

--- code/random.js
+++ code/random.js
@@ -2,7 +2,6 @@ function Random(min, max) {
 	var x = Math.floor((Math.random() * max) + min);
 	return x;
 }
-var x = Random(0,2);
-if (x === 0) { alert('false'); }
-else if (x === 1) { alert('true'); }
-
+var x = Random(1,17);
+var y = x + '.png';
+$(".meme").attr("src",y);
