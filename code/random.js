ba4f3e54b3fdc27edc45fbf15d40b15a1ed57ca7
AwesomeTy79 <32660892+AwesomeTy79@users.noreply.github.com>
2018-04-27 22:21:18 -0600

    Update random.js

--- code/random.js
+++ code/random.js
@@ -2,8 +2,9 @@ function Random(min, max) {
 	var x = Math.floor((Math.random() * max) + min);
 	return x;
 }
-function randmeme {	
+function randmeme() {	
 	var x = Random(1,17);
 	var y = 'memes/' + x + '.png';
 	window.open(y);
 	 }
+randmeme();
